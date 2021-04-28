f68824e541c0b7f8fd65e94f965ebad683726a79
GizmoNours <82664620+GizmoNours@users.noreply.github.com>
2021-04-28 09:52:27 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+require('events').EventEmitter.defaultMaxListeners = 0
+
+client.on("ready", () => {
+    console.log("Prout Prout !");
+});
+
+client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'douanes');
+    if (!channel) return;
+    channel.send(`${member.user} : PROUT !`);
+    
+
+});
+
+client.on("message", msg => {
+    if (msg.author.username != "Kaian"){
+    if (msg.content.match(/Guerre/i)) {
+        msg.reply("Prout !");
+        }
+    
+
+    if (msg.content.match(/Baston/i)) {
+        msg.reply("Prout !");
+     }
+
+    if (msg.content.match(/Prout/i)) {
+        msg.reply("Prout !");
+     }
+    
+
+    if (msg.content.match(/Paix/i)) {
+        msg.reply("Prout !");
+    }
+
+    if (msg.mentions.has(client.user)) {
+        msg.reply('Prout ?');
+    }}
+});
+
+//let port = process.env.PORT || 5000;
+//client.listen(process.env.PORT, '0.0.0.0')
+
+
+client.login(process.env.TOKEN);
+//client.login("");
